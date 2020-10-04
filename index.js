2d91d5e7eb029c82bfe93c2b1004be2a6214ed83
Eugene Verbovoi <63497261+verbovoi@users.noreply.github.com>
2020-10-04 22:57:49 +0300

    Добавляет фичи модального окна

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 import images from './gallery-items.js';
 
+const galleryEl = document.querySelector('.gallery');
+const modalEl = document.querySelector('.js-lightbox');
+const modalImageEl = document.querySelector('.lightbox__image');
+
+//Добавляет картинки в галлерею
+
 const addPicture = (images) => {
     const arrayPictures = [];
-    const listEl = document.querySelector('.gallery');
 
     images.forEach(element => {
         const itemEl = document.createElement('li');
         const imageEl = document.createElement('img');
         imageEl.src = element.preview;
+        imageEl.setAttribute('data-src', element.original);
         imageEl.alt = element.description;
         imageEl.classList.add('gallery__image');
         itemEl.append(imageEl);
         arrayPictures.push(itemEl);
     });
-    listEl.append(...arrayPictures);
+    galleryEl.append(...arrayPictures);
 };
 
 addPicture(images);
+
+
+// Открытие модального окна
+
+galleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    modalEl.classList.add('is-open');
+    console.log(event.target.getAttribute('data-src'));
+
+    modalImageEl.setAttribute('src', event.target.getAttribute('data-src'));
+    modalImageEl.setAttribute('alt', event.target.getAttribute('alt'));
+}
+
+//Закрытие модального окна
+
+modalEl.addEventListener('click', onModalClose);
+
+function onModalClose(event) {
+    if (event.target.nodeName === 'IMG') {
+        return;
+    }
+
+    console.log(event.target.nodeName);
+    modalEl.classList.remove('is-open');
+    modalImageEl.setAttribute('src', '');
+    modalImageEl.setAttribute('alt', '');
+}
